a425be15503d30223adaf67c51d6d61c8cced235
lolitaad1989 <lolitaad19@gmail.com>
2024-05-13 13:48:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "lolita-my-s3-bucket-for-tf-state"
+    bucket = "my-s3-bucket-for-tf-state"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table-tf"
